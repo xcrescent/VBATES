21ca15f717dab79f7c5ed8f0afa156257ae727f5
UTKARSH SINGH <usjadon19@gmail.com>
2023-10-25 01:41:22 +0530

    finished with connections

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,17 +30,11 @@ namespace VBATES
         /// </summary>
         private void InitializeComponent()
         {
-            this.EnquiryTextBox = new System.Windows.Forms.TextBox();
             this.EnquiryButton = new System.Windows.Forms.Button();
             this.ResponseLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // EnquiryTextBox
-            // 
-            this.EnquiryTextBox.Location = new System.Drawing.Point(313, 147);
-            this.EnquiryTextBox.Name = "EnquiryTextBox";
-            this.EnquiryTextBox.Size = new System.Drawing.Size(100, 22);
-            this.EnquiryTextBox.TabIndex = 0;
             // 
             // EnquiryButton
             // 
@@ -50,39 +44,47 @@ namespace VBATES
             this.EnquiryButton.TabIndex = 1;
             this.EnquiryButton.Text = "EnquiryButton";
             this.EnquiryButton.UseVisualStyleBackColor = true;
+            this.EnquiryButton.Click += new System.EventHandler(this.EnquiryButton_Click);
             // 
             // ResponseLabel
             // 
             this.ResponseLabel.AutoSize = true;
-            this.ResponseLabel.Location = new System.Drawing.Point(310, 255);
+            this.ResponseLabel.Location = new System.Drawing.Point(310, 241);
             this.ResponseLabel.Name = "ResponseLabel";
             this.ResponseLabel.Size = new System.Drawing.Size(104, 16);
             this.ResponseLabel.TabIndex = 2;
             this.ResponseLabel.Text = "ResponseLabel";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(251, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 71;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(870, 150);
+            this.dataGridView1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.ResponseLabel);
             this.Controls.Add(this.EnquiryButton);
-            this.Controls.Add(this.EnquiryTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox EnquiryTextBox;
         private System.Windows.Forms.Button EnquiryButton;
         private System.Windows.Forms.Label ResponseLabel;
-
-        
-
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,8 @@ using System.Data.SqlClient;
 using System;
 using System.Windows.Forms;
 using System.Linq;
+using System.Data;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace VBATES
 {
@@ -14,8 +16,9 @@ namespace VBATES
         public Form1()
         {
             InitializeComponent();
-            InitializeSpeechRecognition();
+            
         }
+        private bool isListening = false;
 
         private void InitializeSpeechRecognition()
         {
@@ -36,43 +39,143 @@ namespace VBATES
             };
 
             recognizer.StartContinuousRecognitionAsync().Wait();
+            isListening = true;
         }
+       
+    
+
+    private void AddSampleRow(int trainID, string trainName, string source, string destination, string departureTime, string arrivalTime)
+    {
+        DataGridViewRow row = new DataGridViewRow();
+        row.CreateCells(dataGridView1);
+
+        row.Cells[0].Value = trainID;
+        row.Cells[1].Value = trainName;
+        row.Cells[2].Value = source;
+        row.Cells[3].Value = destination;
+        row.Cells[4].Value = departureTime;
+        row.Cells[5].Value = arrivalTime;
+            if (dataGridView1.InvokeRequired)
+            {
+                dataGridView1.Invoke(new Action(() => { dataGridView1.Rows.Add(row); }));
+            }
+            else
+            {
+                dataGridView1.Rows.Add(row);
+                   
+            }
+    }
+
         private void Recognizer_Recognized(object sender, SpeechRecognitionEventArgs e)
         {
             if (e.Result.Reason == ResultReason.RecognizedSpeech)
             {
                 string recognizedText = e.Result.Text;
+                UpdateResponseLabel(recognizedText);
 
-                // Define some keywords
-                string fromKeyword = "from";
-                string toKeyword = "to";
-                string atKeyword = "at";
+                //string atKeyword = "at";
+                string[] words = recognizedText.Split(' ');
 
-                // Find the positions of keywords
-                int fromIndex = recognizedText.IndexOf(fromKeyword);
-                int toIndex = recognizedText.LastIndexOf(toKeyword);
-                int atIndex = recognizedText.IndexOf(atKeyword);
+                // Find keywords like "from", "to", and "at"
+                int fromIndex = Array.IndexOf(words, "from");
+                int toIndex = Array.LastIndexOf(words, "to");
+                //int atIndex = recognizedText.IndexOf(atKeyword);
 
-                if (fromIndex != -1 && toIndex != -1 && atIndex != -1)
+                if (fromIndex != -1 && toIndex != -1)
                 {
                     // Extract source, destination, and time
-                    string source = recognizedText.Substring(fromIndex + fromKeyword.Length, toIndex - fromIndex - fromKeyword.Length).Trim();
-                    string destination = recognizedText.Substring(toIndex + toKeyword.Length, atIndex - toIndex - toKeyword.Length).Trim();
-                    string time = recognizedText.Substring(atIndex + atKeyword.Length).Trim();
+                    string source = words[fromIndex + 1].Trim().Split('.')[0];
+                    string destination = words[toIndex + 1].Replace(".", "").Replace("?","");
+                    if (fromIndex < toIndex)
+                    {
+                        source = words[fromIndex + 1];
+                        destination = words[toIndex + 1].Replace(".", "").Replace("?", "");
+                    }
+                    else
+                    {
+                        source = words[toIndex + 1];
+                        destination = words[fromIndex + 1].Replace(".", "").Replace("?", "");
+                    }
+                    if (source != "" || destination != "")
+                    {
+                        // Initialize DataGridView columns
+                        //if (dataGridView1.InvokeRequired)
+                        //{
+                        //    dataGridView1.Invoke(new Action(() =>
+                        //    {
+                        //        dataGridView1.Columns.Clear();
+                        //        dataGridView1.Rows.Clear();
+                        //        dataGridView1.Columns.Add("TrainID", "Train ID");
+                        //        dataGridView1.Columns.Add("TrainName", "Train Name");
+                        //        dataGridView1.Columns.Add("SourceStation", "Source Station");
+                        //        dataGridView1.Columns.Add("DestinationStation", "Destination Station");
+                        //        dataGridView1.Columns.Add("DepartureTime", "Departure Time");
+                        //        dataGridView1.Columns.Add("ArrivalTime", "Arrival Time");
+                        //    }));
+                        //
+                        //    // Add sample rows to the DataGridView
+                        //    AddSampleRow(101, "Train X", "Delhi", "Mumbai", "08:00 AM", "12:00 PM");
+                        //    AddSampleRow(102, "Train Y", "Mumbai", "Delhi", "10:00 AM", "02:00 PM");
+                        //
+                        //    UpdateResponseLabel(text: recognizedText);
+                        //    return;
+                        //}
+                        try
+                        {
+                            using (SqlConnection conn = new SqlConnection("Server=localhost;Database=TransportEnquirySystem;Trusted_Connection=True;"))
+                            {
+                                conn.Open();
 
-                    // Now you have the source, destination, and time
-                    Console.WriteLine($"Source: {source}, Destination: {destination}, Time: {time}");
-                    UpdateResponseLabel(text: $"Source: {source}, Destination: {destination}, Time: {time}");
-                }
-                else
-                {
-                    // Handle case where keywords are not found
-                    Console.WriteLine("Command not understood. Please include 'from', 'to', and 'at' in your command.");
-                    UpdateResponseLabel(text: "Command not understood. Please include 'from', 'to', and 'at' in your command.");
+                                string query = "SELECT * FROM TransportInfo WHERE SourceStation = @SourceStation AND DestinationStation = @DestinationStation";
+
+
+                                using (SqlCommand cmd = new SqlCommand(query, conn))
+                                {
+                                    cmd.Parameters.AddWithValue("@SourceStation", source);
+                                    cmd.Parameters.AddWithValue("@DestinationStation", destination);
+                                    Console.WriteLine("Executing SQL commands... "+ source + " " + destination);
+                                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                                    DataTable dataTable = new DataTable();
+                                    adapter.Fill(dataTable);
+                                    foreach (DataRow row in dataTable.Rows)
+                                    {
+                                        foreach (var item in row.ItemArray)
+                                        {
+                                            Console.Write(item + "\t");
+                                        }
+                                        Console.WriteLine();
+                                    }
+                                    if (dataGridView1.InvokeRequired)
+                                    {
+                                        dataGridView1.Invoke(new Action(() => { dataGridView1.DataSource = dataTable; }));
+                                    }
+                                    else
+                                    {
+                                        dataGridView1.DataSource = dataTable;
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error executing SQL commands: " + ex.Message);
+                            MessageBox.Show("Error fetching data: " + ex.Message);
+                        }
+
+                        // Now you have the source, destination, and time
+                        Console.WriteLine($"Source: {source}, Destination: {destination}");
+                        UpdateResponseLabel(text: $"Source: {source}, Destination: {destination}");
+                    }
+                    else
+                    {
+                        
+                        // Handle case where keywords are not found
+                        Console.WriteLine("Command not understood. Please include 'from', 'to', and 'at' in your command.");
+                        UpdateResponseLabel(text: "Command not understood. Please include 'from', 'to', and 'at' in your command.");
+                    }
                 }
             }
         }
-
         private void Speak(string text)
         {
             SpeechConfig speechConfig = SpeechConfig.FromSubscription("ef675da96ee740639a0cac547c370f5d", "southeastasia");
@@ -100,6 +203,47 @@ namespace VBATES
             }
 
         }
+        private void ExecuteSQLButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost;Database=TransportEnquirySystem;Trusted_Connection=True;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sourceStation = "Mumbai";
+                    string destinationStation = "Delhi";
+                    string departureTime = "10:00 AM";
+                    string arrivalTime = "02:00 PM";
+                    string trainName = "Train Y";
+                    using (SqlCommand cmd = new SqlCommand(
+                    "IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'TransportInfo') " +
+                    "CREATE TABLE TransportInfo ( ID INT PRIMARY KEY IDENTITY(1,1), SourceStation VARCHAR(100), DestinationStation VARCHAR(100), DepartureTime TIME, ArrivalTime TIME, TrainName VARCHAR(100))", connection))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand("INSERT INTO TransportInfo (SourceStation, DestinationStation, DepartureTime, ArrivalTime, TrainName) VALUES (@SourceStation, @DestinationStation, @DepartureTime, @ArrivalTime, @TrainName)", connection))
+                    {
+                        cmd.Parameters.AddWithValue("@SourceStation", sourceStation);
+                        cmd.Parameters.AddWithValue("@DestinationStation", destinationStation);
+                        cmd.Parameters.AddWithValue("@DepartureTime", departureTime);
+                        cmd.Parameters.AddWithValue("@ArrivalTime", arrivalTime);
+                        cmd.Parameters.AddWithValue("@TrainName", trainName);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                    
+                    Console.WriteLine("SQL commands executed successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error executing SQL commands: " + ex.Message);
+            }
+        }
+
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -108,27 +252,56 @@ namespace VBATES
 
         private void EnquiryButton_Click(object sender, EventArgs e)
         {
-            string query = EnquiryTextBox.Text;
-            Console.WriteLine(query);   
-            StoreInDatabase(query);
-            Console.WriteLine(query);
-            string voiceResponse = "Recognized: " + query;
-            ResponseLabel.Text = voiceResponse;
-            Speak(voiceResponse);
-        }
 
-
-        private void UpdateResponseLabel(string text)
-        {
-            if (ResponseLabel.InvokeRequired)
+            //ExecuteSQLButton_Click(sender,e);
+            if (isListening)
             {
-                ResponseLabel.Invoke(new Action(() => { ResponseLabel.Text = text; }));
+                StopListening();
+                UpdateEnquiryButton("Start Voice Search");
             }
             else
             {
-                ResponseLabel.Text = text;
+                InitializeSpeechRecognition();
+                UpdateEnquiryButton("Stop Voice Search");
+            }
+            //            string voiceResponse = "Recognized: " + query;
+
+        }
+        // Function to stop listening
+        private void StopListening()
+        {
+            if (recognizer != null && isListening)
+            {
+                recognizer.StopContinuousRecognitionAsync().Wait();
+                isListening = false;
+                Console.WriteLine("Listening stopped.");
             }
         }
+
+
+            private void UpdateResponseLabel(string text)
+            {
+                if (ResponseLabel.InvokeRequired)
+                {
+                    ResponseLabel.Invoke(new Action(() => { ResponseLabel.Text = text; }));
+                }
+                else
+                {
+                    ResponseLabel.Text = text;
+                }
+            }
+
+            private void UpdateEnquiryButton(string text)
+        {
+                if (EnquiryButton.InvokeRequired)
+            {
+                    EnquiryButton.Invoke(new Action(() => { EnquiryButton.Text = text; }));
+                }
+                else
+            {
+                    EnquiryButton.Text = text;
+                }
+            }   
 
 
     }
