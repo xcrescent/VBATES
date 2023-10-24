8fd34a8e45fad58063907046b316cbb3649629bf
UTKARSH SINGH <usjadon19@gmail.com>
2023-10-25 00:05:36 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace VBATES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EnquiryTextBox = new System.Windows.Forms.TextBox();
+            this.EnquiryButton = new System.Windows.Forms.Button();
+            this.ResponseLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EnquiryTextBox
+            // 
+            this.EnquiryTextBox.Location = new System.Drawing.Point(313, 147);
+            this.EnquiryTextBox.Name = "EnquiryTextBox";
+            this.EnquiryTextBox.Size = new System.Drawing.Size(100, 22);
+            this.EnquiryTextBox.TabIndex = 0;
+            // 
+            // EnquiryButton
+            // 
+            this.EnquiryButton.Location = new System.Drawing.Point(313, 195);
+            this.EnquiryButton.Name = "EnquiryButton";
+            this.EnquiryButton.Size = new System.Drawing.Size(100, 23);
+            this.EnquiryButton.TabIndex = 1;
+            this.EnquiryButton.Text = "EnquiryButton";
+            this.EnquiryButton.UseVisualStyleBackColor = true;
+            // 
+            // ResponseLabel
+            // 
+            this.ResponseLabel.AutoSize = true;
+            this.ResponseLabel.Location = new System.Drawing.Point(310, 255);
+            this.ResponseLabel.Name = "ResponseLabel";
+            this.ResponseLabel.Size = new System.Drawing.Size(104, 16);
+            this.ResponseLabel.TabIndex = 2;
+            this.ResponseLabel.Text = "ResponseLabel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResponseLabel);
+            this.Controls.Add(this.EnquiryButton);
+            this.Controls.Add(this.EnquiryTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox EnquiryTextBox;
+        private System.Windows.Forms.Button EnquiryButton;
+        private System.Windows.Forms.Label ResponseLabel;
+
+        
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System.Data.SqlClient;
+using System;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace VBATES
+{
+    public partial class Form1 : Form
+    {
+
+        private SpeechRecognizer recognizer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSpeechRecognition();
+        }
+
+        private void InitializeSpeechRecognition()
+        {
+            var config = SpeechConfig.FromSubscription("ef675da96ee740639a0cac547c370f5d", "southeastasia");
+            recognizer = new SpeechRecognizer(config);
+            Console.WriteLine("Say something...");
+            recognizer.Recognized += (s, e) =>
+            {
+                if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    string query = e.Result.Text;
+                    //StoreInDatabase(query);
+                    string voiceResponse = "Recognized: " + query;
+                    
+                    Recognizer_Recognized(s, e);
+                    Speak(voiceResponse);
+                }
+            };
+
+            recognizer.StartContinuousRecognitionAsync().Wait();
+        }
+        private void Recognizer_Recognized(object sender, SpeechRecognitionEventArgs e)
+        {
+            if (e.Result.Reason == ResultReason.RecognizedSpeech)
+            {
+                string recognizedText = e.Result.Text;
+
+                // Define some keywords
+                string fromKeyword = "from";
+                string toKeyword = "to";
+                string atKeyword = "at";
+
+                // Find the positions of keywords
+                int fromIndex = recognizedText.IndexOf(fromKeyword);
+                int toIndex = recognizedText.LastIndexOf(toKeyword);
+                int atIndex = recognizedText.IndexOf(atKeyword);
+
+                if (fromIndex != -1 && toIndex != -1 && atIndex != -1)
+                {
+                    // Extract source, destination, and time
+                    string source = recognizedText.Substring(fromIndex + fromKeyword.Length, toIndex - fromIndex - fromKeyword.Length).Trim();
+                    string destination = recognizedText.Substring(toIndex + toKeyword.Length, atIndex - toIndex - toKeyword.Length).Trim();
+                    string time = recognizedText.Substring(atIndex + atKeyword.Length).Trim();
+
+                    // Now you have the source, destination, and time
+                    Console.WriteLine($"Source: {source}, Destination: {destination}, Time: {time}");
+                    UpdateResponseLabel(text: $"Source: {source}, Destination: {destination}, Time: {time}");
+                }
+                else
+                {
+                    // Handle case where keywords are not found
+                    Console.WriteLine("Command not understood. Please include 'from', 'to', and 'at' in your command.");
+                    UpdateResponseLabel(text: "Command not understood. Please include 'from', 'to', and 'at' in your command.");
+                }
+            }
+        }
+
+        private void Speak(string text)
+        {
+            SpeechConfig speechConfig = SpeechConfig.FromSubscription("ef675da96ee740639a0cac547c370f5d", "southeastasia");
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer(speechConfig);
+            synthesizer.SpeakTextAsync(text).Wait();
+        }
+
+        private void StoreInDatabase(string query)
+        {
+            using (SqlConnection conn = new SqlConnection("Server=localhost;Database=TransportEnquirySystem;Trusted_Connection=True;"))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(
+                    "IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Enquiries') " +
+                    "CREATE TABLE Enquiries (Id INT PRIMARY KEY IDENTITY(1,1), Query NVARCHAR(MAX), Response NVARCHAR(MAX))", conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Enquiries (Query) VALUES (@Query)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@Query", query);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            recognizer.StopContinuousRecognitionAsync().Wait();
+        }
+
+        private void EnquiryButton_Click(object sender, EventArgs e)
+        {
+            string query = EnquiryTextBox.Text;
+            Console.WriteLine(query);   
+            StoreInDatabase(query);
+            Console.WriteLine(query);
+            string voiceResponse = "Recognized: " + query;
+            ResponseLabel.Text = voiceResponse;
+            Speak(voiceResponse);
+        }
+
+
+        private void UpdateResponseLabel(string text)
+        {
+            if (ResponseLabel.InvokeRequired)
+            {
+                ResponseLabel.Invoke(new Action(() => { ResponseLabel.Text = text; }));
+            }
+            else
+            {
+                ResponseLabel.Text = text;
+            }
+        }
+
+
+    }
+}
